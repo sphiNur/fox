5b0dfdad4a94ab948ee6803612760cf3467c2421
sphiNx <12168474+sphinxr@user.noreply.gitee.com>
2024-08-15 16:33:50 +0500

    change build and preview outDir to docs

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'docs'
+  },
+  preview: {
+    outDir: 'docs',
   }
 })
