6fa36e025ce28df1924cd2911fc06d291a2e43d8
sphiNx <12168474+sphinxr@user.noreply.gitee.com>
2024-08-19 17:57:57 +0500

    summary page 添加缓冲逻辑 daily page 添加提交等待反馈

--- src/pages/Daily.vue
+++ src/pages/Daily.vue
@@ -12,9 +12,17 @@ const data = ref({
 });
 
 const date = ref('')
+const modal_box_status = ref(true)
+const modal_box_title = ref('')
+const modal_box_content = ref('')
+let timeoutId = null
 
 async function onSubmit() {
-  // toast.value.status = Object.values(data.value).some(value => typeof value === 'number' || isNaN(value));
+  modal_box_status.value = true
+
+  const dataContent = Object.values(data.value).some(value => typeof value !== 'number' || isNaN(value) || /^[+-]?\d+(\.\d+)?$/.test(value.toString()));
+  console.log(dataContent);
+
   const body = JSON.stringify({
     bank: data.value.bank,
     payment: data.value.payment,
@@ -25,8 +33,19 @@ async function onSubmit() {
     date: date.value,
   })
 
-  if (date.value.length !== 0) await fetchData(body)
-  else console.log("date is invalid")
+  if (date.value.length !== 0) {
+    timeoutId = setTimeout( async () => {
+      await fetchData(body)
+    })
+  } else if(dataContent) {
+    modal_box_status.value = false
+    modal_box_title.value = "Notice"
+    modal_box_content.value = "Please just type the number."
+  } else {
+    modal_box_status.value = false
+    modal_box_title.value = "Notice"
+    modal_box_content.value = "Date is invalid, please select a date."
+  }
 }
 
 const fetchData = async (body) => {
@@ -36,10 +55,21 @@ const fetchData = async (body) => {
       body: body
     });
     const data = await res.json();
-
-    console.log(data)
+    if (data.message === 'Success') {
+      clearTimeout(timeoutId.value)
+      modal_box_status.value = false
+      modal_box_title.value = "Success"
+      modal_box_content.value = "Pales check summary page."
+    } else {
+      modal_box_status.value = false
+      modal_box_title.value = "Field"
+      modal_box_content.value = "Failed response."
+    }
   } catch (err) {
-    console.log(err);
+    clearTimeout(timeoutId); // Ensure timeout is cleared on error
+    modal_box_status.value = false; // Hide loading spinner
+    modal_box_title.value = "Error";
+    modal_box_content.value = "An error occurred. Please try again.";
   }
 }
 </script>
@@ -50,11 +80,34 @@ const fetchData = async (body) => {
   </div>
   <br/>
   <div class="flex flex-row w-full">
-    <input class="w-1/2" type="date" v-model="date">
-    <button class="btn w-1/2" @click="onSubmit">Submit</button>
+    <input class="w-1/2" type="date" v-model="date" placeholder="select date" />
+    <button class="btn w-1/2" @click="onSubmit" onclick="my_modal_5.showModal()">Submit</button>
   </div>
+
+  <!-- Open the modal using ID.showModal() method -->
+  <dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
+    <div class="modal-box">
+      <div class="modal-content" v-show="modal_box_status === false">
+        <h3 class="text-lg font-bold">{{modal_box_title}}</h3>
+        <p class="py-4">{{modal_box_content}}</p>
+        <div class="modal-action">
+          <form method="dialog">
+            <!-- if there is a button in form, it will close the modal -->
+            <button class="btn">Close</button>
+          </form>
+        </div>
+      </div>
+      <div class="modal-load" v-show="modal_box_status === true">
+        <span class="loading loading-infinity loading-lg"></span>
+      </div>
+    </div>
+  </dialog>
 </template>
 
 <style scoped>
-
+.modal-load {
+  display: flex;
+  justify-content: center; /* 水平居中 */
+  align-items: center;     /* 垂直居中 */
+}
 </style>
--- src/pages/Summary.vue
+++ src/pages/Summary.vue
@@ -1,24 +1,39 @@
 <script setup>
-import {onMounted, ref} from 'vue';
+import {computed, onMounted, ref} from 'vue';
 
 const data = ref([]); // Define a ref to store the fetched data
 
 // Define an async function to fetch data
 const fetchData = async () => {
   try {
-    const response = await fetch("https://grammybot.sphinxr.workers.dev/api/get/demo", {
-      method: "GET",
-    });
-
-    data.value = await response.json(); // Assign the fetched data to the ref
-    // Assuming insert_date is in ISO format or another valid date string format
-    data.value.sort((a, b) => {
-      const dateA = new Date(a.insert_date); // Parse date for item a
-      const dateB = new Date(b.insert_date); // Parse date for item b
-      return dateA - dateB; // Compare dates to sort in ascending order
-    });
+    const response = await fetch("https://grammybot.sphinxr.workers.dev/api/get/demo",{method: "GET"});
+    const jsonData = await response.json();
+    jsonData.sort((a, b) => new Date(a.insert_date) - new Date(b.insert_date));
+    data.value = jsonData;
   } catch (error) {
     console.error('Error fetching data:', error);
+  }
+};
+
+const confirmed_data = computed(() => data.value.filter(item => item.is_confirmed === 1));
+const unconfirmed_data = computed(() => data.value.filter(item => item.is_confirmed === 0));
+
+const onConfirm = async (item) => {
+  try {
+    const response = await fetch(`https://grammybot.sphinxr.workers.dev/api/post/updateConfirm/demo`, {
+      method: 'POST',
+      body: JSON.stringify(item)
+    });
+    // 更新本地数据
+    const jsonData = await response.json();
+
+    if (jsonData.message.success) {
+      item.is_confirmed = 1;
+    } else {
+      console.log(jsonData);
+    }
+  } catch (error) {
+    console.error('更新项时出错:', error);
   }
 };
 
@@ -27,8 +42,8 @@ onMounted(fetchData);
 </script>
 
 <template>
-  <div class="overflow-x-auto">
-    <table class="table table-xs table-pin-rows table-pin-cols">
+  <div class="max-h-96 overflow-x-auto">
+    <table class="table table-zebra table-xs table-pin-rows table-pin-cols">
       <thead>
       <tr>
         <th>Date</th>
@@ -41,7 +56,7 @@ onMounted(fetchData);
       </tr>
       </thead>
       <tbody>
-      <tr v-for="(item, index) in data" :key="index">
+      <tr v-for="(item, index) in confirmed_data" :key="index">
         <th>{{ item.insert_date }}</th>
         <td>{{ item.bank }}</td>
         <td>{{ item.payment }}</td>
@@ -54,6 +69,37 @@ onMounted(fetchData);
     </table>
   </div>
   <br/>
+  <div class="max-h-48 overflow-x-auto">
+    <div></div>
+    <table class="table table-zebra table-xs table-pin-rows table-pin-cols">
+      <thead>
+      <tr>
+        <th>Date</th>
+        <td>Bank</td>
+        <td>Payment</td>
+        <td>Salary</td>
+        <td>Market</td>
+        <td>Bank Cash</td>
+        <td>Cash</td>
+        <td>Confirm</td>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(item, index) in unconfirmed_data" :key="index">
+        <th>{{ item.insert_date }}</th>
+        <td>{{ item.bank }}</td>
+        <td>{{ item.payment }}</td>
+        <td>{{ item.salary }}</td>
+        <td>{{ item.market }}</td>
+        <td>{{ item.bank_cash }}</td>
+        <td>{{ item.cash }}</td>
+        <td>
+          <button class="btn btn-outline btn-warning btn-xs" @click="onConfirm(item)">OK</button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style scoped>
